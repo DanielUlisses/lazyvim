1c9313a87cc819581c2edf7e3696fb8b84440735
DanielUlisses <danielulisses@outlook.com>
2024-05-08 13:01:35 -0300

    Update example.lua

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -41,34 +41,19 @@ return {
   },
 
   -- change some telescope options and a keymap to browse plugin files
-  {
+   {
     "nvim-telescope/telescope.nvim",
     dependencies = {
-      {
         "nvim-telescope/telescope-fzf-native.nvim",
-        build =
-        "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
-      },
+        build = "make",
     },
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    },
-    -- change some options
     opts = {
       defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
+        path_display = {shorten = 3},
+        prompt_prefix = "> ",
       },
+      extensions_list = {"fzf", "terms", "themes"},
     },
-  },
 
   -- add pyright to lspconfig
   {
